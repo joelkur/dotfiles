e0e9d4ce447fecac6aaeea4bd549c41dd6209f13
joelkur <jkuronen95@gmail.com>
2022-07-05 20:38:55 +0300

    Add snippet for twind class definition

--- nvim/.config/nvim/lua/snippets.lua
+++ nvim/.config/nvim/lua/snippets.lua
@@ -9,11 +9,23 @@ local dynamic = ls.dynamic_node
 
 ls.add_snippets("typescriptreact", {
   snip({
-    trig = "ujsx h",
+    trig = "ujsx",
     namr = "Use jsx in fresh component",
     dscr = "Tells fresh that jsx is used in this file"
   }, {
     text({"/** @jsx h */", "import { h } from \"preact\""}),
+  })
+})
+
+ls.add_snippets("typescriptreact", {
+  snip({
+    trig = "tw",
+    namr = "Twind class",
+    dscr = "Twind class definition"
+  }, {
+    text("class={tw`"),
+    insert(1, ""),
+    text("`}"),
   })
 })
 

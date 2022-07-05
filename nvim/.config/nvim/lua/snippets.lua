68debe8b5a13956d5f394159da949a12e898c4eb
joelkur <jkuronen95@gmail.com>
2022-07-05 15:33:41 +0300

    Add snippet

--- nvim/.config/nvim/lua/snippets.lua
+++ nvim/.config/nvim/lua/snippets.lua
@@ -1,22 +1,19 @@
--- local ls = require'luasnip'
--- local snip = ls.snippet
--- local node = ls.snippet_node
--- local text = ls.text_node
--- local insert = ls.insert_node
--- local func = ls.function_node
--- local choice = ls.choice_node
--- local dynamic = ls.dynamic_node
--- 
--- ls.add_snippets("typescriptreact", {
---   snip({
---     trig = "imp",
---     namr = "Import",
---     dscr = "Import statement",
---   }, {
---     text("import "),
---     insert(1, "module"),
---     text(" from \""),
---     insert(2, "library"),
---     text("\""),
---   })
--- })
+local ls = require'luasnip'
+local snip = ls.snippet
+local node = ls.snippet_node
+local text = ls.text_node
+local insert = ls.insert_node
+local func = ls.function_node
+local choice = ls.choice_node
+local dynamic = ls.dynamic_node
+
+ls.add_snippets("typescriptreact", {
+  snip({
+    trig = "ujsx h",
+    namr = "Use jsx in fresh component",
+    dscr = "Tells fresh that jsx is used in this file"
+  }, {
+    text({"/** @jsx h */", "import { h } from \"preact\""}),
+  })
+})
+

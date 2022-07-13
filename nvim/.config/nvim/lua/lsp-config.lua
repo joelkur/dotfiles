0c344c5e19e51366d05180c160a5e0098546ca7f
joelkur <jkuronen95@gmail.com>
2022-07-13 18:59:42 +0300

    Add html and css lsp config

--- nvim/.config/nvim/lua/lsp-config.lua
+++ nvim/.config/nvim/lua/lsp-config.lua
@@ -100,3 +100,13 @@ nvim_lsp.rust_analyzer.setup {
   on_attach = on_attach,
   capabilities = capabilities,
 }
+
+nvim_lsp.html.setup {
+  on_attach = on_attach,
+  capabilities = capabilities
+}
+
+nvim_lsp.cssls.setup {
+  on_attach = on_attach,
+  capabilities = capabilities
+}

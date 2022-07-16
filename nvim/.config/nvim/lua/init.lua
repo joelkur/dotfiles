e3911b879943cb7b235638f9e72edb1f36bf9352
joelkur <jkuronen95@gmail.com>
2022-07-16 16:58:46 +0300

    Change vimscript init to lua

--- nvim/.config/nvim/lua/init.lua
+++ nvim/.config/nvim/lua/init.lua
@@ -1,3 +1,4 @@
+require "plugins-config"
 require "lsp-config"
 require "cmp-config"
 require "telescope-config"
---
+++ nvim/.config/nvim/lua/plugins-config.lua
@@ -0,0 +1,35 @@
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+end
+
+
+return require('packer').startup(function(use)
+  use 'williamboman/nvim-lsp-installer'
+  use 'neovim/nvim-lspconfig'
+  use 'leafgarland/typescript-vim'
+  use 'maxmellon/vim-jsx-pretty'
+  use 'nvim-lua/plenary.nvim'
+  use 'nvim-telescope/telescope.nvim'
+  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+  use 'hrsh7th/nvim-cmp'
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'saadparwaiz1/cmp_luasnip'
+  use 'L3MON4D3/LuaSnip' 
+  use { 'folke/tokyonight.nvim', branch = 'main' }
+  use 'NLKNguyen/papercolor-theme'
+  use 'arcticicestudio/nord-vim'
+  use 'ellisonleao/gruvbox.nvim'
+  use 'tpope/vim-surround'
+  use 'kyazdani42/nvim-web-devicons' -- optional, for file icons
+  use 'kyazdani42/nvim-tree.lua'
+  use 'folke/lua-dev.nvim'
+  use 'kdheepak/lazygit.nvim'
+  use 'rafamadriz/friendly-snippets'
+  use { 'iamcco/coc-tailwindcss', run = 'yarn install --frozen-lockfile && yarn run build' }
+
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)

2db918dc05bb3eb6313709db79b5b4ef0965caa0
joelkur <jkuronen95@gmail.com>
2023-04-11 00:01:37 +0300

    add black to null-ls config

--- astronvim/.config/astronvim/lua/user/plugins/mason.lua
+++ astronvim/.config/astronvim/lua/user/plugins/mason.lua
@@ -24,7 +24,7 @@ return {
     "jay-babu/mason-null-ls.nvim",
     -- overrides `require("mason-null-ls").setup(...)`
     opts = {
-      ensure_installed = { "stylua", "prettier" },
+      ensure_installed = { "stylua", "prettier", "black" },
     },
   },
   {

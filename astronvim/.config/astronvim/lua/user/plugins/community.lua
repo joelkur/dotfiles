f4a46b474837674a9f473804d121a382e88468cc
joelkur <jkuronen95@gmail.com>
2023-04-10 22:32:47 +0300

    add glow

--- astronvim/.config/astronvim/lua/user/plugins/community.lua
+++ astronvim/.config/astronvim/lua/user/plugins/community.lua
@@ -10,4 +10,5 @@ return {
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.color.modes-nvim" },
   { import = "astrocommunity.color.twilight-nvim" },
+  { import = "astrocommunity.markdown-and-latex.glow-nvim" },
 }

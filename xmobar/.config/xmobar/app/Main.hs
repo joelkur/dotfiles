77db531a82d5761f31ea69150992bb87d967114f
joelkur <jkuronen95@gmail.com>
2023-11-05 12:11:50 +0200

    add xmobar

---
+++ xmobar/.config/xmobar/app/Main.hs
@@ -0,0 +1,22 @@
+module Main where
+
+import Xmobar
+
+config :: Config
+config =
+  defaultConfig
+    { font = "CaskaydiaCove Nerd Font 13"
+    , allDesktops = True
+    , alpha = 200
+    , commands =
+        [ Run XMonadLog
+        , Run $ Memory ["t", "Mem: <usedratio>%"] 10
+        , Run $ Kbd []
+        , Run $ Date "%a %_d %b %Y <fc=#ee9a00>%H:%M:%S</fc>" "date" 10
+        ]
+    , template = "%XMonadLog% }{ %kbd% | %date% | %memory%"
+    , alignSep = "}{"
+    }
+
+main :: IO ()
+main = xmobar config

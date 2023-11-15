b8b73e8bbdd17c17fd0f190e704c4aa4ad91d6f5
joelkur <jkuronen95@gmail.com>
2023-11-15 08:34:10 +0200

    add smart borders

--- xmonad/.config/xmonad/xmonad.hs
+++ xmonad/.config/xmonad/xmonad.hs
@@ -1,8 +1,8 @@
 import XMonad
-
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.EwmhDesktops (ewmh, ewmhFullscreen)
 import XMonad.Hooks.StatusBar (defToggleStrutsKey, statusBarProp, withEasySB)
+import XMonad.Layout.NoBorders
 import XMonad.Layout.Spacing (spacingWithEdge)
 import XMonad.Layout.ThreeColumns
 import XMonad.Util.EZConfig
@@ -10,23 +10,23 @@ import XMonad.Util.Loggers
 import XMonad.Util.Run (hPutStrLn, spawnPipe)
 import XMonad.Util.SpawnOnce (spawnOnce)
 
-myLayout = tiled ||| Mirror tiled ||| Full ||| threeCol
- where
-  threeCol = ThreeColMid nmaster delta ratio
-  tiled = Tall nmaster delta ratio
-  nmaster = 1
-  ratio = 1 / 2
-  delta = 3 / 100
+myLayout = smartBorders $ spacingWithEdge 10 $ tiled ||| Mirror tiled ||| Full ||| threeCol
+  where
+    threeCol = ThreeColMid nmaster delta ratio
+    tiled = Tall nmaster delta ratio
+    nmaster = 1
+    ratio = 1 / 2
+    delta = 3 / 100
 
 myConfig =
   def
-    { modMask = mod4Mask
-    , terminal = "kitty"
-    , layoutHook = spacingWithEdge 10 myLayout
-    , borderWidth = 2
-    , startupHook = myStartupHook
-    , focusedBorderColor = "#ea6962"
-    , normalBorderColor = "#d4be98"
+    { modMask = mod4Mask,
+      terminal = "kitty",
+      layoutHook = myLayout,
+      borderWidth = 2,
+      startupHook = myStartupHook,
+      focusedBorderColor = "#ea6962",
+      normalBorderColor = "#d4be98"
     }
     `additionalKeysP` [ ("M-S-b", spawn "brave-browser")
                       ]

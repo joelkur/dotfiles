f39f3e5c0a9fd077901dcb07bad87f63cf51c12e
joelkur <jkuronen95@gmail.com>
2023-12-19 08:24:47 +0200

    improve todo comments keymaps

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -42,3 +42,39 @@ require('which-key').register({
   ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
   ['<leader>f'] = { name = '[F]iles', _ = 'which_key_ignore' },
 })
+
+-- Telescope keymaps
+-- See `:help telescope.builtin`
+vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
+vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
+vim.keymap.set('n', '<leader>/', function()
+  -- You can pass additional configuration to telescope to change theme, layout, etc.
+  require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+    winblend = 10,
+    previewer = false,
+  })
+end, { desc = '[/] Fuzzily search in current buffer' })
+
+vim.keymap.set("n", "<C-H>", "<C-W>h", { desc = "Switch to left window" })
+vim.keymap.set("n", "<C-J>", "<C-W>j", { desc = "Switch to below window" })
+vim.keymap.set("n", "<C-K>", "<C-W>k", { desc = "Switch to above window" })
+vim.keymap.set("n", "<C-L>", "<C-W>l", { desc = "Switch to right window" })
+
+vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
+vim.keymap.set('n', '<leader>gg', ":LazyGit<CR>", { desc = 'Start Lazy[G]it' })
+vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
+vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
+vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
+vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
+vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
+vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
+vim.keymap.set('n', '<leader>ft', ":Telescope file_browser path=%:p:h select_buffer=true<CR>",
+  { desc = 'Telescope file browser' })
+vim.keymap.set('n', '<leader>fb', ":NvimTreeToggle<CR>",
+  { desc = 'Toggle NvimTree' })
+
+-- Todo-comments keymaps
+local tc = require 'todo-comments'
+vim.keymap.set('n', ']t', tc.jump_next, { desc = "Jump to next todo comment" })
+vim.keymap.set('n', '[t', tc.jump_prev, { desc = "Jump to previous todo comment" })
+vim.keymap.set('n', '<leader>st', tc.jump_prev, { desc = "Jump to previous todo comment" })
